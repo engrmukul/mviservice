914a7a3c1e7e0b07c4e163686a9cd7d2728b127c
Mizanur Rahaman <mizan@ticonsys.com>
2019-11-21 12:36:17 +0600

    bug fix

--- app/controller/registrationController.js
+++ app/controller/registrationController.js
@@ -2,24 +2,6 @@
 const Registration = require("../models/registrationModel");
 
 exports.createRegistration = (req, res) => {
-
-
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-      }
-    
-      // The name of the input field (i.e. "UploadPicture") is used to retrieve the uploaded file
-      let UploadPicture = req.files.UploadPicture;
-    
-      // Use the mv() method to place the file somewhere on your server
-      UploadPicture.mv('/somewhere/on/your/server/filename.jpg', function(err) {
-        if (err)
-          return res.status(500).send(err);
-    
-        res.send('File uploaded!');
-      });
-
-
     return (new Registration()).createRegistration(req)
     .then((registration) => {
         res.status(200).send({
